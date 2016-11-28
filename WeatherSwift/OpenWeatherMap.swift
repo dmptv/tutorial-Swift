acf9cb194e630bea5be362d1038e1813bdfb5ae9
dmptv <dimatixon@Dimas-MacBook-Air.local>
2016-11-28 17:51:54 +0600

    set tab bar

--- WeatherSwift/OpenWeatherMap.swift
+++ WeatherSwift/OpenWeatherMap.swift
@@ -8,57 +8,132 @@
 
 import Foundation
 import UIKit
+import Alamofire
+import CoreLocation
 
+protocol OpenWeatherMapDelegate {
+    // reqired
+    func updateWeatherInfo(weatherJson: JSON)
+    func failure()
+}
+
+ // модель погоды
 class OpenWeatherMap {
-
-    var nameCity: String
-    var temp: Int
+    let weatherUrl = "http://api.openweathermap.org/data/2.5/weather"
+    var delagete: OpenWeatherMapDelegate!
     
-    var description: String
-    var currentTime: String?
-    var icon: UIImage?
     
-    init(weatherJson: NSDictionary) {
-        nameCity = weatherJson["name"] as! String
-        let main = weatherJson["main"] as! NSDictionary
-        temp = main["temp"] as! Int
-        
-        let weather = weatherJson["weather"] as! NSArray
-        let zero = weather[0] as! NSDictionary
-        description = zero["description"] as! String
-        let dt = weatherJson["dt"] as! Int
-        currentTime = timeFromUnix(unixTime: dt)
-        
-        let strIcon = zero["icon"] as! String
-        icon = weatherIcon(stringIcon: strIcon)
-        
+    func weatherFor(city: String) {
+        // вместо init  
+        let params = ["q" : city, "APPID" : "3aeb0927332507b5d1c47e8cccb8c7b9"]
+        setRequest(params: params as [String : AnyObject]?)
     }
     
+    func weatherFor(geo: CLLocationCoordinate2D) {
+        let params = ["lat": geo.latitude, "lon": geo.longitude, "APPID" : "3aeb0927332507b5d1c47e8cccb8c7b9"] as [String : Any]
+        setRequest(params: params as [String : AnyObject]?)
+    }
+    
+    func setRequest(params: [String : AnyObject]?) {
+        request(weatherUrl, method: .get, parameters: params).responseJSON { (res) in
+            if (res.result.error != nil) {
+                self.delagete.failure()
+            } else {
+                let weatherJson = JSON(res.result.value as Any)
+                
+                DispatchQueue.main.async {
+                    self.delagete.updateWeatherInfo(weatherJson: weatherJson)
+                }
+            }
+        }
+    }
+    
+    
+    // из секунд делаем строку
     func timeFromUnix(unixTime: Int) -> String {
         let timeInsecond = TimeInterval(unixTime)
         let weatherDate = Date(timeIntervalSince1970: timeInsecond)
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "HH:mm"
-        
         return dateFormatter.string(from: weatherDate)
     }
     
-    func weatherIcon(stringIcon: String) -> UIImage {
-        
-        let imageName: String
-        
-        switch stringIcon {
-        case "01d": imageName = "06.png"
-        case "04n": imageName = "08.png"
-        default: imageName = "01"
+    
+    func updateWeatherIcon(condition: Int, nightTime: Bool) -> UIImage {
+        var imageName = String()
+        switch (condition, nightTime) {
+            // Thunderstorm
+            case let (x,y) where x < 300 && y == true: imageName = "11n"
+            case let (x,y) where x < 300 && y == false: imageName = "11d"
+            // Drizzle
+            case let (x,y) where x < 500 && y == true: imageName = "09n"
+             case let (x,y) where x < 300 && y == false: imageName = "09d"
+            //Rain
+            case let (x,y) where x <= 504 && y == true: imageName = "10n"
+            case let (x,y) where x <= 504 && y == false: imageName = "10d"
+            
+            case let (x,y) where x == 511 && y == true: imageName = "13n"
+            case let (x,y) where x == 511 && y == false: imageName = "13d"
+            
+            case let (x,y) where x < 600 && y == true: imageName = "09n"
+            case let (x,y) where x < 600 && y == false: imageName = "09d"
+            // Snow
+            case let (x,y) where x < 700 && y == true: imageName = "13n"
+            case let (x,y) where x < 700 && y == false: imageName = "13d"
+            // Atmosthere
+            case let (x,y) where x < 800 && y == true: imageName = "50n"
+            case let (x,y) where x < 800 && y == false: imageName = "50d"
+            // Clouds
+            case let (x,y) where x == 800 && y == true: imageName = "01n"
+            case let (x,y) where x == 800 && y == false: imageName = "01d"
+            
+            case let (x,y) where x == 801 && y == true: imageName = "02n"
+            case let (x,y) where x == 801 && y == false: imageName = "02d"
+            
+            case let (x,y) where x > 802 || x < 804 && y == true: imageName = "03n"
+            case let (x,y) where x > 802 || x < 804 && y == false: imageName = "03d"
+            
+            case let (x,y) where x == 804 && y == true: imageName = "04n"
+            case let (x,y) where x == 804 && y == false: imageName = "04d"
+            // Additional
+            case let (x,y) where x < 100 && y == true: imageName = "11n"
+            case let (x,y) where x < 100 && y == false: imageName = "11d"
+            
+            case (_,_): imageName = "none"
         }
-        
-        var iconImage = UIImage(named: imageName)
-        return iconImage!
+        let iconImage = UIImage(named: imageName)
+        return (iconImage)!
     }
     
+    
+    func convertTemperature(contry: String, temperature: Double) -> Double {
+        if (contry == "US") {
+            // convert to Farengate
+            return round(((temperature - 273.215) * 1.8) + 32)
+        } else {
+            // convert to Cel
+            return round(temperature - 273.15)
+        }
+    }
+    
+    
+    func isTimeNight(weaatherJson: JSON) -> Bool {
+        var nightTime = false
+        let nowTime = NSDate().timeIntervalSince1970
+        let sunrise = weaatherJson["sys"]["sunrise"].doubleValue
+        let sunset = weaatherJson["sys"]["sunset"].doubleValue
+        
+        if (nowTime < sunrise) || (nowTime > sunset) {
+            nightTime = true
+        }
+        return nightTime
+    }
 }
+
+
+
+
 
 
 
--- WeatherSwift/ViewController.swift
+++ WeatherSwift/ViewController.swift
@@ -7,59 +7,171 @@
 //
 
 import UIKit
+import CoreLocation
 
 
+// "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=3aeb0927332507b5d1c47e8cccb8c7b9"
 
-class ViewController: UIViewController { // 3aeb0927332507b5d1c47e8cccb8c7b9
+class ViewController: UIViewController, OpenWeatherMapDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var speedWindLabel: UILabel!
+    @IBOutlet weak var humodotyLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
-    let url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=3aeb0927332507b5d1c47e8cccb8c7b9"
-
+    var openWeather = OpenWeatherMap() // сделали экзэмпляр класса OpenWeatherMap (владелец протокола)
+    var hud = MBProgressHUD()
+    let locationManager: CLLocationManager = CLLocationManager()
+    
+    @IBAction func cityTappedButton(_ sender: UIBarButtonItem) {
+        displayCity()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let stringURL = URL(string: url)
         
-//        let weatherObject = NSData(contentsOf: stringURL!)
-//        print(weatherObject as Any)
+        // Set background
+        let imageName = "1bg.jpg"
+        let bg = UIImage(named: imageName)
+        self.view.backgroundColor = UIColor(patternImage: bg!)
 
-        // мы здесь на главном потоке
-        let session = URLSession.shared
+        // Set setup
+        self.openWeather.delagete = self
         
-        let task = session.downloadTask(with: stringURL!) {
-            (location:  URL?, response: URLResponse?, error: Error?) in
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    
+    func displayCity()  {
+        let alert = UIAlertController(title: "City",
+                                      message: "enter name City",
+                                      preferredStyle: UIAlertControllerStyle.alert)
+        
+        let cancel = UIAlertAction(title: "cancel",
+                                   style: UIAlertActionStyle.cancel,
+                                   handler: nil)
+        
+        alert.addAction(cancel)
+        
+        let ok = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action)->Void in
             
-            do {
-            let weatherData = try Data(contentsOf: stringURL!)      
-            
-            let weatherJSON = try JSONSerialization.jsonObject(with: weatherData, options: JSONSerialization.ReadingOptions.allowFragments) as! Dictionary<String, Any>
-                
-                let weather = OpenWeatherMap(weatherJson: weatherJSON as NSDictionary) // в модель передали дикт
-                
-                print(weather.nameCity)
-                print(weather.temp)
-                print(weather.description)
-                print(weather.currentTime!)
-                print(weather.icon!)
-                
-                // изм на UI надо делать на главн потоке
-                DispatchQueue.main.async {
-                    self.iconImageView.image = weather.icon
-                }
-                
-                
-                
-                
-            } catch {
-                print("json error: \(error)")
+            if let textField = (alert.textFields?.first) as UITextField! {
+                self.activityIndicator()
+                self.openWeather.weatherFor(city: textField.text!) // загрузка данных
             }
             
         }
         
+        alert.addAction(ok)
+        
+        alert.addTextField { (textFiled)->Void in // добавили тексфилд в алерт
+            textFiled.placeholder = "City Name"
+        }
+        
+        self.present(alert, animated: true, completion: nil) // презентуем алерт контролер
+    }
+    
+    
+    func activityIndicator() {
+        hud.label.text = "Loading..."
+        hud.backgroundColor = UIColor.gray
+        hud.alpha = 0.6
+        self.view.addSubview(hud)
+        hud.show(animated: true)
+    }
+    
+    
+    //Mark: OpenWeatherMapDelegate
+    
+         // required метод для делегата
+    func updateWeatherInfo(weatherJson: JSON) {
+        hud.hide(animated: true)
+        
+        if let tempResult = weatherJson["main"]["temp"].double {
+            
+            // Get country
+            let country = weatherJson["sys"]["country"].stringValue
+            
+            // Get city name
+            let cityName = weatherJson["name"].stringValue
+            self.cityNameLabel.text = "\(cityName), \(country)"
+            
+            // Get time
+            let time = weatherJson["dt"].intValue
+            let timeToStr = openWeather.timeFromUnix(unixTime: time)
+            self.timeLabel.text = "At \(timeToStr) it is"
+            
+            // Get convert temperature
+            let temperature = openWeather.convertTemperature(contry: country, temperature: tempResult)
+            self.tempLabel.text = "\(temperature)"
+            
+            // get icon
+            let weather = weatherJson["weather"][0]
+            let condition = weather["id"].intValue
+            let nightTime = openWeather.isTimeNight(weaatherJson: weatherJson)
+            let icon = openWeather.updateWeatherIcon(condition: condition, nightTime: nightTime)
+            
+            // Get description
+            let descr = weather["description"].stringValue
+            self.descriptionLabel.text = descr
+            
+            // Get speed wind
+            let speed = weatherJson["wind"]["speed"].doubleValue
+            self.speedWindLabel.text = "\(speed)"
+            
+            // Get humididty
+            let humidity = weatherJson["main"]["humidity"].intValue
+            self.humodotyLabel.text = "\(humidity)"
+            
+            self.iconImageView.image = icon
+        } else {
+            print("Unable load weather info")
+        }
+    }
+    
+    
+    func failure() {
+        // No connection
+        let networkController = UIAlertController(title: "Error",
+                                                  message: "No connection!",
+                                                  preferredStyle: UIAlertControllerStyle.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
+        
+        networkController.addAction(okButton)
+        self.present(networkController, animated: true, completion: nil)
+    }
+    
+    
+    //MARK: - CLLocationManagerDelegate
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(manager.location!)
+        
+        self.activityIndicator()
+        
+        let currentLocation = locations.last as CLLocation!
+        
+        if (currentLocation!.horizontalAccuracy > 0) {
+            // Stop update locations to save battery life
+            locationManager.stopUpdatingLocation()
+            
+            let coords = CLLocationCoordinate2DMake((currentLocation?.coordinate.latitude)!,
+                                                    (currentLocation?.coordinate.longitude)!)
+            self.openWeather.weatherFor(geo: coords)
+            print(coords)
+        }
+    }
+    
 
-        task.resume()
-
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        print("can't get your location")
     }
 
 }
